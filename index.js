adb654854cb81aab340406597e70431c74b7de79
Akalanka Perera <akalanka@parallax.lk>
2022-03-10 21:20:52 +0530

    Added error handling

--- index.js
+++ index.js
@@ -7,19 +7,22 @@ const initialize = async (token) => {
   axios.setHeaders({ ...axios.instance.defaults.headers, Authorization: `Bearer ${token}` });
 };
 
-const getOrganizationLeaderboard = async (orgs, filters) => {
- const contributions = await utils.getPaginatedResults(githubService.getOrganizationPRs, orgs, filters);
- return leaderboardService.generateLeaderboardRecords(contributions);
+const getOrganizationLeaderboard = async (orgs, filters = {}) => {
+ try {
+    const contributions = await utils.getPaginatedResults(githubService.getOrganizationPRs, orgs, filters);
+    return leaderboardService.generateLeaderboardRecords(contributions);
+ } catch (e) {
+    return e
+ }
 };
 
-const getRepositoryLeaderboard = async (owner, repoName, filters) => {
-  const contributions = await utils.getPaginatedResults(githubService.getRepositoryPRs, [], filters, owner, repoName);
-  return leaderboardService.generateLeaderboardRecords(contributions);
+const getRepositoryLeaderboard = async (owner, repoName, filters = {}) => {
+  try {
+    const contributions = await utils.getPaginatedResults(githubService.getRepositoryPRs, [], filters, owner, repoName);
+    return leaderboardService.generateLeaderboardRecords(contributions);
+  } catch (e) {
+    return e
+  }
 };
 
-
-initialize("token_goes_here");
-getOrganizationLeaderboard(['sliit-foss'], {dateRange: '2021-10-14..2021-10-30'}).then((ans) => console.log(ans));
-// getRepositoryLeaderboard('Akalanka47000', 'portfolio', {dateRange: '2021-10-14..2021-10-30'}).then((ans) => console.log(ans));
-
-module.exports = { getOrganizationLeaderboard, getRepositoryLeaderboard };
+module.exports = { initialize, getOrganizationLeaderboard, getRepositoryLeaderboard };
