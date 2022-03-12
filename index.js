a426b111bbc07604df13ac900bbf147479e3e04a
Akalanka Perera <akalanka@parallax.lk>
2022-03-12 13:12:09 +0530

    Added usage instructions (#2)

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const initialize = async (token) => {
   axios.setHeaders({ ...axios.instance.defaults.headers, Authorization: `Bearer ${token}` });
 };
 
-const getOrganizationLeaderboard = async (orgs, filters = {}) => {
+const getOrganizationLeaderboard = async ({orgs, filters = {}}) => {
  try {
     const contributions = await utils.getPaginatedResults(githubService.getOrganizationPRs, orgs, filters);
     return leaderboardService.generateLeaderboardRecords(contributions);
@@ -16,7 +16,7 @@ const getOrganizationLeaderboard = async (orgs, filters = {}) => {
  }
 };
 
-const getRepositoryLeaderboard = async (owner, repoName, filters = {}) => {
+const getRepositoryLeaderboard = async ({owner, repoName, filters = {}} ) => {
   try {
     const contributions = await utils.getPaginatedResults(githubService.getRepositoryPRs, [], filters, owner, repoName);
     return leaderboardService.generateLeaderboardRecords(contributions);
